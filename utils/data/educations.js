3b1aaa9701747499aeb8cc419db45bac8d5190b8
Saad Masood <sm22ae@herts.ac.uk>
2025-12-18 02:05:14 +0000

    new UI

---
+++ utils/data/educations.js
@@ -0,0 +1,14 @@
+export const educations = [
+  {
+    id: 1,
+    title: "MSc Software Engineering with Advanced Research",
+    duration: "2022 - 2024",
+    institution: "University of Hertfordshire - United Kingdom",
+  },
+  {
+    id: 2,
+    title: "Bachelors of Science in Computer Sciences",
+    duration: "2014 - 2018",
+    institution: "Pak-AIMS - The Institute of Management Sciences - Pakistan",
+  },
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Saad Masood",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Saad Masood. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'saadmasood353@gmail.com',
+  phone: '+447424953271',
+  address: 'London, United Kingdom',
+  facebook: 'https://www.facebook.com/saad.masood.3152',
+  leetcode: "https://leetcode.com/u/saadmasood353/",
+  devUsername: "saad",
+  github: 'https://github.com/usermigrated',
+  linkedIn: 'https://www.linkedin.com/in/saad-masood-192210160/',
+  twitter: 'https://x.com/saadifalcon',
+  stackOverflow: 'https://stackoverflow.com/users/19445361/saad-masood',
+  resume: "https://docs.google.com/document/d/1m4IKa2mcIB8re--hv1DG0zBb36G71voZ/view"
+}

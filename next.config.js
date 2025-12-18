1a8532708f12707f49b1c27e7e0c4c46f96d9131
Saad Masood <sm22ae@herts.ac.uk>
2025-12-18 02:13:31 +0000

    fixes

--- next.config.js
+++ next.config.js
@@ -1,26 +1,36 @@
-const path = require('path')
+// const path = require('path')
  
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media.dev.to',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media2.dev.to',
+//         pathname: '**',
+//       },
+//     ],
+//   },
+// }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        pathname: '**',
-      },
-    ],
+    unoptimized: true,
   },
-}
+};
+
+module.exports = nextConfig;

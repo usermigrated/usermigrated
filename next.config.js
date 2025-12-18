fc8102dcc9c3f07c0fbfd5ef660bad0d2aac8cec
Saad Masood <sm22ae@herts.ac.uk>
2025-12-18 02:45:05 +0000

    fixes

--- next.config.js
+++ next.config.js
@@ -1,40 +1,26 @@
-// const path = require('path')
+const path = require('path')
  
-// module.exports = {
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, 'styles')],
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'res.cloudinary.com',
-//         pathname: '**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'media.dev.to',
-//         pathname: '**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'media2.dev.to',
-//         pathname: '**',
-//       },
-//     ],
-//   },
-// }
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // required for static export
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
-  // optional: fix routing if your repo is not at the root of the domain
-  // basePath: "/<repo-name>",
-  // assetPrefix: "/<repo-name>/",
-};
-
-module.exports = nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.dev.to',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media2.dev.to',
+        pathname: '**',
+      },
+    ],
+  },
+}

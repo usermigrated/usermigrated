c3096e5280d5968cd0b3d92b8e1b4ef347cf7303
Saad Masood <sm22ae@herts.ac.uk>
2026-06-07 05:16:07 +0100

    Updates

--- _next/static/wvNNtkWXZKXRVH5Xcc61J/_buildManifest.js
+++ _next/static/wvNNtkWXZKXRVH5Xcc61J/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/usermigrated//_next/:path+",
-        "destination": "/usermigrated/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
